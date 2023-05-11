0f41486cfb81d0fc521578ec122729a29fc9a7fb
Nicolas Lautaro Herrera <98717285+Zambottinico@users.noreply.github.com>
2023-05-10 22:12:37 -0300

    first commit

---
+++ scripts/animations.js
@@ -0,0 +1,26 @@
+ScrollReveal().reveal('.navbar',{
+    duration:3000,
+    origin: 'bottom',
+    distance:'-100px'
+});
+
+ScrollReveal().reveal('.rowA',{
+    duration:2000,
+    origin: 'top',
+    distance:'-100px'
+});
+
+ScrollReveal().reveal('.footer',{
+    duration:2000,
+    origin: 'top',
+    distance:'-100px'
+});
+ScrollReveal().reveal('.home',{
+    duration:2000,
+    origin: 'top',
+    distance:'-100px'
+});
+
+
+
+
