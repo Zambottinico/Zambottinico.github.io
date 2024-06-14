13d1fbad75284f52f7b7804b5dafbefc7a4c355d
Herrera-Nicolas <zambottinico@gmail.com>
2024-06-13 23:34:32 -0300

    add portafolio v2

--- scripts/animations.js
+++ scripts/animations.js
@@ -1,26 +1,28 @@
-ScrollReveal().reveal('.navbar',{
-    duration:3000,
-    origin: 'bottom',
-    distance:'-100px'
+ScrollReveal().reveal(".navbar", {
+  duration: 3000,
+  origin: "bottom",
+  distance: "-100px",
 });
 
-ScrollReveal().reveal('.rowA',{
-    duration:2000,
-    origin: 'top',
-    distance:'-100px'
+ScrollReveal().reveal(".rowA", {
+  duration: 2000,
+  origin: "top",
+  distance: "-100px",
+  beforeReveal: function (el) {
+    el.style.transform = "none";
+  },
 });
 
-ScrollReveal().reveal('.footer',{
-    duration:2000,
-    origin: 'top',
-    distance:'-100px'
+ScrollReveal().reveal(".footer", {
+  duration: 2000,
+  origin: "top",
+  distance: "-100px",
 });
-ScrollReveal().reveal('.home',{
-    duration:2000,
-    origin: 'top',
-    distance:'-100px'
+ScrollReveal().reveal(".home", {
+  duration: 2000,
+  origin: "top",
+  distance: "-100px",
+  beforeReveal: function (el) {
+    el.style.transform = "none";
+  },
 });
-
-
-
-
