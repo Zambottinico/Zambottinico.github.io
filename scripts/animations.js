57ecf59784ef45c93e456cd11d82d5cd021bf53a
Herrera-Nicolas <zambottinico@gmail.com>
2024-06-26 12:54:45 -0300

    Update portafolio to v2

--- scripts/animations.js
+++ scripts/animations.js
@@ -2,6 +2,9 @@ ScrollReveal().reveal(".navbar", {
   duration: 3000,
   origin: "bottom",
   distance: "-100px",
+  beforeReveal: function (el) {
+    el.style.transform = "none";
+  },
 });
 
 ScrollReveal().reveal(".rowA", {
