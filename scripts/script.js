b8fe9390151f2678c14421bf8d1479aae6008810
Herrera-Nicolas <zambottinico@gmail.com>
2024-06-27 19:05:48 -0300

    add button styles

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,6 @@
-
-
-
-$(document).ready(function() {
-    // Cerrar el menú al hacer clic en un enlace
-    $('.navbar-nav a').on('click', function(){
-        $('.navbar-collapse').collapse('hide');
-    });
+$(document).ready(function () {
+  // Cerrar el menú al hacer clic en un enlace
+  $(".navbar-nav a").on("click", function () {
+    $(".navbar-collapse").collapse("hide");
+  });
 });
-
-
-
-
-
